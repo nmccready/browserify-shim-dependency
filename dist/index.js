dc9971a86aaad2eb572364039d7081883b06218b
Nick McCready <nmccready@gmail.com>
2014-08-16 12:59:20 -0400

    bower updates

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
  *
  * @version: 0.0.1
  * @author: Nicholas McCready
- * @date: Sat Aug 16 2014 12:22:09 GMT-0400 (EDT)
+ * @date: Sat Aug 16 2014 12:57:50 GMT-0400 (EDT)
  * @license: MIT
  */
 var blockExport = true;
